f04caa2ab6b45888e68691e5e0d010bba82c08c2
ednelsonoliveira <ednelsonoliveira@gmail.com>
2012-09-06 20:28:44 -0300

    renomendo automaton pra transition_function

--- lib/conversor.rb
+++ lib/conversor.rb
@@ -1,84 +1,64 @@
 # encoding: utf-8 
 # teste de github
 class Conversor
-  attr_reader :automaton
-  attr_reader :new_initial_state
+  attr_reader :transition_function, :initial_state, :alphabet, :final_states
 
   def initialize(automaton = {}, alphabet = [], final_states = [], initial_state=[])
-    @automaton = automaton                     
+    @transition_function = automaton                     
     @alphabet = alphabet 
     @initial_state = initial_state
-    @new_initial_state = []
     @final_states = final_states
-    @new_final_states = []
   end                     
   
-  def eclose(state, states=@automaton)  
-    remove_empty_states_when_there_are_many_states(  (state+states[state]['']).uniq )
+  def eclose(state, transition_function=@transition_function)  
+     (state+transition_function[state]['']).uniq 
   end
   
   def to_dfa
-     dfa = {}                   
-     @new_initial_state = eclose(@initial_state)                                                          
-     insert_new_state(dfa, @new_initial_state) 
-     @new_final_states = dfa_final_state(dfa)   
-     dfa
+     dfa_transition_function = {} 
+     dfa_alphabet = @alphabet.select{|symbol| symbol!=''}                                                                            
+     insert_new_state( dfa_transition_function, dfa_alphabet, eclose(@initial_state),@transition_function)   
+     Conversor.new( dfa_transition_function, dfa_alphabet, dfa_final_states(dfa_transition_function,@final_states), eclose(@initial_state) )
   end  
-  
-  def final_states_after_processing
-    @new_final_states.sort
-  end
 
-  def insert_new_state(hash, state)
+  def insert_new_state( dfa_transition_function, alphabet, state, transition_function) 
 
-    transitions = transitions(state, @alphabet.select {|item| item != ''} , @automaton)
+    dfa_transition_function[state] = transitions(state, alphabet , transition_function)
 
-    hash[state] = transitions
-
-    transitions.each_pair do |key, value|
-      if(!hash.has_key?(value))
-        insert_new_state(hash, value)
+    dfa_transition_function[state].each_pair do |symbol, reached_state|
+      if(!dfa_transition_function.has_key?(reached_state))
+        insert_new_state(dfa_transition_function, alphabet, reached_state, transition_function)
       end
     end
-
   end
 
-  def transitions( state, alphabet, automaton)
-	transitions = alphabet.map{|symbol| extended_transition(state, symbol, automaton)}
+  def transitions( state, alphabet, transition_function)
+	transitions = alphabet.map{|symbol| transition(state, symbol, transition_function)}
 	Hash[transitions]
   end
 
-  def extended_transition( states, symbol, automaton)
-	next_states = states.flat_map{ |state| eclose([state], automaton)}.flat_map{ |state| automaton[[state]][symbol]}.flat_map{ |state| eclose([state],automaton)}.sort.uniq 
-	[symbol, remove_empty_states_when_there_are_many_states(next_states)]
+  def transition( states, symbol, transition_function)
+	reached_states = states.flat_map{ |state| eclose([state], transition_function)}.flat_map{ |state| transition_function[[state]][symbol]}.flat_map{ |state| eclose([state],transition_function)}.uniq.sort 
+	[symbol, reached_states]
   end
 
-  def remove_empty_states_when_there_are_many_states(states)
-    	(states.size>1)? states.select{|state| state!=:ø} : states
-  end
-
-  def dfa_final_state(hash)
-    	hash.keys.select{|state| @final_states.one?{|final_state| ! (final_state & state).empty? } }
-  end
-  
-  def array_to_hash_symbol(array)
-      array.sort.map{|sym| sym.to_s}.join("_").to_sym
+  def dfa_final_states(transition_function,final_states)
+    	transition_function.keys.select{|state| final_states.one?{|final_state| ! (final_state & state).empty? } }.uniq.sort
   end
 
   def to_nfa
 	nfa_alphabet = @alphabet.select{|symbol| symbol!=''}
-	Conversor.new(nfa_automaton(@automaton, nfa_alphabet), nfa_alphabet, nfa_final_states(@automaton, @final_states), @initial_state)
+	Conversor.new(nfa_transition_function(@transition_function, nfa_alphabet), nfa_alphabet, nfa_final_states(@transition_function, @final_states), @initial_state)
   end
   
-  def nfa_automaton(old_automaton, alphabet)
+  def nfa_transition_function(old_automaton, alphabet)
 	nfa_states = old_automaton.keys.map{|state| [state, transitions(state, alphabet, old_automaton)]}
 	Hash[nfa_states]
   end
 
-  def nfa_final_states(automaton, final_states)
-	( automaton.keys.select{|state| ! ( eclose(state,automaton) & final_states ).empty? } + final_states ).uniq		
+  def nfa_final_states(transition_function, final_states)
+	( transition_function.keys.select{|state| ! ( eclose(state,transition_function) & final_states ).empty? } + final_states ).uniq.sort		
   end	
-
 end   
 
 
