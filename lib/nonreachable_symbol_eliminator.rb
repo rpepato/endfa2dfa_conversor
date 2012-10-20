a892d68710bd4e9614f6705db3714a832a81f6e1
ednelsonoliveira <ednelsonoliveira@gmail.com>
2012-10-19 23:59:12 -0300

    renomeando metodo

--- lib/nonreachable_symbol_eliminator.rb
+++ lib/nonreachable_symbol_eliminator.rb
@@ -1,17 +1,17 @@
 # encoding: utf-8 
 module NonreachableSymbolEliminator
 
-	def reachable_symbols2( symbol, terminals, reachable_symbols, productions )
+	def reachable_symbols_util( symbol, terminals, reachable_symbols, productions )
 		if( terminals.include?(symbol) and ! reachable_symbols.include?(symbol) )
 			reachable_symbols << symbol
 		elsif( ! reachable_symbols.include?(symbol) )
-			productions[symbol].each{|body| body.each{|inner_symbol| reachable_symbols2( inner_symbol, terminals, reachable_symbols << symbol, productions ) }}					
+			productions[symbol].each{|body| body.each{|inner_symbol| reachable_symbols_util( inner_symbol, terminals, reachable_symbols << symbol, productions ) }}					
 		end
 	end
 
 	def reachable_symbols( initial_symbol, terminals , productions )
 		reachable_symbols = []
-		reachable_symbols2( initial_symbol, terminals, reachable_symbols, productions )
+		reachable_symbols_util( initial_symbol, terminals, reachable_symbols, productions )
 		reachable_symbols.uniq
 	end
 
