6aae8e59f675cc3e094598f7b5f6186ad970d45d
Roberto Pepato <rpepato@gmail.com>
2015-08-12 14:32:33 -0300

    Add gemfile and configure rspec

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ endfa2dfa_conversor
 ===================
 
 A conversor for a non-deterministic finite automaton with epsilon transitions to a deterministic finite automaton
+
+See the specs folder to understand how to use it.
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
 # encoding: utf-8
+RSpec.configure do |config|
+  config.color = true
+  config.tty = true
+  config.formatter = :documentation
+end
 puts File.expand_path(File.join('..', 'lib'), File.dirname(__FILE__))
 $LOAD_PATH << File.expand_path(File.join('..', 'lib'), File.dirname(__FILE__))
 __END__
